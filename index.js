74c05875cef24e49e8a060d6f25e5fc71fccdd60
Turkane Yusifzade <your-yusifzadeturkanee@gmail.com>
2024-11-25 12:41:59 +0400

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const apiUrl = 'https://v6.exchangerate-api.com/v6/' + apiKey + '/latest/';
 let fromCurrency = 'RUB'; 
 let toCurrency = 'USD'; 
 
+function checkInternetConnection() {
+    const noInternetElement = document.getElementById('no-internet');
+
+    if (navigator.onLine) {
+noInternetElement.style.display = 'none';
+    } else {
+     noInternetElement.style.display = 'block';
+    }
+}
+
+
+window.addEventListener('online', checkInternetConnection);
+window.addEventListener('offline', checkInternetConnection);
+
+document.addEventListener('DOMContentLoaded', checkInternetConnection);
+
 async function getExchangeRate(fromCurrency, toCurrency) {
     try {
         const response = await fetch(apiUrl + fromCurrency); 
@@ -17,29 +33,27 @@ function updateConversionRates() {
     getExchangeRate(fromCurrency, toCurrency).then(rate => {
         const reverseRate = 1 / rate;
     
-document.querySelector('.iHavehad .conversion-rate').textContent = `1 ${fromCurrency} = ${rate.toFixed(5)} ${toCurrency}`;    
-  
-     document.querySelector('.iWantToGet .conversion-rate').textContent = `1 ${toCurrency} = ${reverseRate.toFixed(5)} ${fromCurrency}`;
+        document.querySelector('.iHavehad .conversion-rate').textContent = `1 ${fromCurrency} = ${rate.toFixed(5)} ${toCurrency}`;    
+        document.querySelector('.iWantToGet .conversion-rate').textContent = `1 ${toCurrency} = ${reverseRate.toFixed(5)} ${fromCurrency}`;
     });
 }
 
 function updateConvertedValue(changedInput) {
     const amountFromInput = document.querySelector('.iHavehad .amount-display input');
-const amountToInput = document.querySelector('.iWantToGet .amount-display input');
+    const amountToInput = document.querySelector('.iWantToGet .amount-display input');
     
- if (changedInput === 'from') {
+    if (changedInput === 'from') {
         const amountFrom = parseFloat(amountFromInput.value);
- if (!isNaN(amountFrom)) {
+        if (!isNaN(amountFrom)) {
             getExchangeRate(fromCurrency, toCurrency).then(rate => {
-     if (!rate) return;
-         const convertedAmount = amountFrom * rate; 
+                if (!rate) return;
+                const convertedAmount = amountFrom * rate; 
                 amountToInput.value = convertedAmount.toFixed(2); 
-  });
+            });
         } else {
             amountToInput.value = '';
         }
     } else if (changedInput === 'to') {
-      
         const amountTo = parseFloat(amountToInput.value);
         if (!isNaN(amountTo)) {
             getExchangeRate(toCurrency, fromCurrency).then(rate => {
@@ -53,40 +67,33 @@ const amountToInput = document.querySelector('.iWantToGet .amount-display input'
     }
 }
 
-
 document.querySelectorAll('.iHavehad .currency-selector button').forEach(button => {
     button.addEventListener('click', function() {
-        fromCurrency = button.textContent.trim();
+fromCurrency = button.textContent.trim();
         
-    
         document.querySelectorAll('.iHavehad .currency-selector button').forEach(btn => {
             btn.classList.remove('active');
-        });
+});
 
-     
-        button.classList.add('active');
+     button.classList.add('active');
         
-        updateConversionRates(); 
+ updateConversionRates(); 
         updateConvertedValue('from'); 
     });
 });
 
 document.querySelectorAll('.iWantToGet .currency-selector button').forEach(button => {
-    button.addEventListener('click', function() {
-        toCurrency = button.textContent.trim(); 
-        
-        document.querySelectorAll('.iWantToGet .currency-selector button').forEach(btn => {
-            btn.classList.remove('active');
+ button.addEventListener('click', function() {
+        toCurrency = button.textContent.trim();  
+ document.querySelectorAll('.iWantToGet .currency-selector button').forEach(btn => {
+     btn.classList.remove('active');
         });
-
-       
-        button.classList.add('active');
+button.classList.add('active');
         
         updateConversionRates(); 
-        updateConvertedValue('to')
+        updateConvertedValue('to');
     });
 });
-
 
 document.querySelector('.iHavehad .amount-display input').addEventListener('input', function() {
     updateConvertedValue('from');
@@ -95,6 +102,5 @@ document.querySelector('.iHavehad .amount-display input').addEventListener('inpu
 document.querySelector('.iWantToGet .amount-display input').addEventListener('input', function() {
     updateConvertedValue('to');
 });
-
 
 updateConversionRates();
